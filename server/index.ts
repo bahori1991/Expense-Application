f69917003d485965d23057d3a1bff17fb643d059
bahori1991 <bahori2011ms@gmail.com>
2025-01-13 20:36:10 +0900

    basic hono api

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,15 @@
-import { Hono } from "hono"
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { expenseRoute } from "./expenses";
 
 const app = new Hono();
+
+app.use("*", logger());
+
+app.get("/test", (c) => {
+  return c.json({ message: "test" });
+});
+
+app.route("/api/expenses", expenseRoute);
 
 export default app;
